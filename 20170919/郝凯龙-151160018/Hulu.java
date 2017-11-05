d78f8876becc63470d0ba3b8c73cb39b05b5a4f2
Hao-Kailong <m13951723179@163.com>
2017-11-05 20:08:02 +0800

    修改：每个类应该分开放在不同的文件中

---
+++ 20170919/郝凯龙-151160018/Hulu.java
@@ -0,0 +1,21 @@
+
+
+class Hulu{
+	private int key;
+	private String rank;
+	private String color;
+	public Hulu(int key,String rank,String color){
+		this.key=key;
+		this.rank=rank;
+		this.color=color;
+	}
+	public int getKey(){
+		return this.key;
+	}
+	public String getRank(){
+		return this.rank;
+	}
+	public String getColor(){
+		return this.color;
+	}
+}
