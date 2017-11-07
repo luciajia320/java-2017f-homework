eb5c50d7b9994c27164cdefd08090a50d0ac3ebd
Ginna <2569500918@qq.com>
2017-11-07 15:44:08 +0800

    第二次与第三次作业总和

---
+++ 20170919/151220092-石瑾/huluwa.java
@@ -0,0 +1,43 @@
+
+public class huluwa {
+	int age;
+	huluwa(){
+		age=0;
+	}
+	void setage(int age) {
+		this.age=age;
+	}
+	void printcolor() {
+		if(age==1)
+			System.out.print("赤");
+		else if(age==2)
+			System.out.print("橙");
+		else if(age==3)
+			System.out.print("黄");
+		else if(age==4)
+			System.out.print("绿");
+		else if(age==5)
+			System.out.print("青");
+		else if(age==6)
+			System.out.print("蓝");
+		else if(age==7)
+			System.out.print("紫️");
+	}
+	void printname() {
+		if(age==1)
+			System.out.print("老大");
+		else if(age==2)
+			System.out.print("老二");
+		else if(age==3)
+			System.out.print("老三");
+		else if(age==4)
+			System.out.print("老四");
+		else if(age==5)
+			System.out.print("老五");
+		else if(age==6)
+			System.out.print("老六");
+		else if(age==7)
+			System.out.print("老七️");
+	};
+	
+}
