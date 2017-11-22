f4fe50930d09df84bf0ae44570096cecb71c318f
zengbingjie <zengbingjie@zbj-mac.local>
2017-11-22 22:14:10 +0800

    hw3 v3.0 generics

--- 20171010/曾冰洁-151220004/Position.java
+++ 20171010/曾冰洁-151220004/Position.java
@@ -1,17 +1,17 @@
-public class Position {
+public class Position<T extends Creature> {
 
     private Coord coord;
-    private Creature holder;
+    private T holder;
 
     public Position(Coord coord){
         this.coord = coord;
     }
 
-    public Creature getHolder() {
+    public T getHolder() {
         return holder;
     }
 
-    public void setHolder(Creature holder) {
+    public void setHolder(T holder) {
         this.holder = holder;
     }
 
