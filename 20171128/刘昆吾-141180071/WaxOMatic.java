7b35990ac6fe9127eff9c249f4a35b0074b78199
qiuming <1546223488@qq.com>
2018-01-01 19:39:54 +0800

    第四次作业Lamda表达式版本

--- 20171128/刘昆吾-141180071/WaxOMatic.java
+++ 20171128/刘昆吾-141180071/WaxOMatic.java
@@ -54,103 +54,6 @@ class Car {
 
 }
 
-
-
-class WaxOn implements Runnable {
-
-    private Car car;
-    private int num;
-
-
-
-    public WaxOn(Car c, int num) {
-
-        car = c;
-        this.num = num;
-
-    }
-
-
-
-    public void run() {
-
-        try {
-
-            
-            while (!Thread.interrupted()) {
-            	
-            	synchronized(car) {
-            	
-            	 car.waitForBuffing();
-            
-                printnb("WaxOn" + num + ":" + "Wax On!\n ");
-
-                TimeUnit.MILLISECONDS.sleep(200);
-
-                car.waxed();
-
-            }
-            
-            }
-
-        } catch (InterruptedException e) {
-
-            print("Exiting via interrupt");
-
-        }
-
-        print("Ending Wax On task");
-
-    }
-
-}
-
-
-
-class WaxOff implements Runnable {
-
-    private Car car;
-
-
-
-    public WaxOff(Car c) {
-
-        car = c;
-
-    }
-
-
-
-    public void run() {
-  
-        try {
-
-            while (!Thread.interrupted()) {
-
-                car.waitForWaxing();
-
-                printnb("Wax Off!\n ");
-
-                TimeUnit.MILLISECONDS.sleep(200);
-
-                car.buffed();
-
-            }
-
-        } catch (InterruptedException e) {
-
-            print("Exiting via interrupt");
-
-        }
-
-        print("Ending Wax Off task");
-
-    }
-
-}
-
-
-
 public class WaxOMatic {
 
     public static void main(String[] args) throws Exception {
@@ -158,12 +61,90 @@ public class WaxOMatic {
         Car car = new Car();
 
         ExecutorService exec = Executors.newCachedThreadPool();
-
-        exec.execute(new WaxOff(car));
-
-        exec.execute(new WaxOn(car, 1));
         
-        exec.execute(new WaxOn(car, 2));
+        exec.execute(() -> {
+        	 try {
+
+                 while (!Thread.interrupted()) {
+
+                     car.waitForWaxing();
+
+                     printnb("Wax Off!\n ");
+
+                     TimeUnit.MILLISECONDS.sleep(200);
+
+                     car.buffed();
+
+                 }
+
+             } catch (InterruptedException e) {
+
+                 print("Exiting via interrupt");
+
+             }
+
+             print("Ending Wax Off task");
+        });
+        
+        exec.execute(() -> {
+        	try {
+
+                int num = 1;
+                while (!Thread.interrupted()) {
+                	
+                	synchronized(car) {
+                	
+                	 car.waitForBuffing();
+                
+                    printnb("WaxOn" + num + ":" + "Wax On!\n ");
+
+                    TimeUnit.MILLISECONDS.sleep(200);
+
+                    car.waxed();
+
+                }
+                
+                }
+
+            } catch (InterruptedException e) {
+
+                print("Exiting via interrupt");
+
+            }
+
+            print("Ending Wax On task");
+
+        });
+        
+        exec.execute(() -> {
+        	try {
+
+                int num = 2;
+                while (!Thread.interrupted()) {
+                	
+                	synchronized(car) {
+                	
+                	 car.waitForBuffing();
+                
+                    printnb("WaxOn" + num + ":" + "Wax On!\n ");
+
+                    TimeUnit.MILLISECONDS.sleep(200);
+
+                    car.waxed();
+
+                }
+                
+                }
+
+            } catch (InterruptedException e) {
+
+                print("Exiting via interrupt");
+
+            }
+
+            print("Ending Wax On task");
+
+        });
 
         TimeUnit.SECONDS.sleep(5); // Run for a while...
 
