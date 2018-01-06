b0e445cbfc03017756026d71e65e175f62b045f4
tinax <Tinax@tinax.local>
2018-01-06 09:40:16 +0800

    final final final

--- Fianl Project/薛雨婷-151220138/huluwa/src/main/java/nju/huluwa/Layout.java
+++
@@ -1,8 +0,0 @@
-package nju.huluwa;
-
-import java.util.ArrayList;
-
-//布阵！布阵
-public interface Layout {
-    void move(ArrayList<ArrayList<Position>> positions,int toX,int toY);
-}
--- Fianl Project/薛雨婷-151220138/huluwa/src/main/java/nju/huluwa/Main.java
+++
@@ -1,8 +0,0 @@
-package nju.huluwa;
-
-public class Main {
-    public static void main(String[] args) {
-        Ground ground = new Ground();
-        ground.setVisible(true);
-    }
-}
